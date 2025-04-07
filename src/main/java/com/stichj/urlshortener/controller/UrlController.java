82ea563bd05e310563651a10dfb2bfe823014ddc
stichj <jonas.stich@tum.de>
2025-04-07 17:39:35 +0200

    Implement Rest Controller for creating short codes and retrieving original urls from short codes with redirects

---
+++ src/main/java/com/stichj/urlshortener/controller/UrlController.java
@@ -0,0 +1,37 @@
+package com.stichj.urlshortener.controller;
+
+import com.stichj.urlshortener.model.UrlMapping;
+import com.stichj.urlshortener.service.UrlService;
+import com.stichj.urlshortener.util.exceptions.UrlNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UrlController {
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @PostMapping("/shorten")
+    public ResponseEntity<UrlMapping> shortenUrl(@RequestBody String originalUrl) {
+        UrlMapping mapping = urlService.shortenUrl(originalUrl);
+
+        return new ResponseEntity<>(mapping, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> getOriginalUrl(@PathVariable String shortCode) {
+        try {
+            String originalUrl = urlService.getOriginalUrl(shortCode);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Location", originalUrl);
+            return new ResponseEntity<>(headers, HttpStatus.FOUND);
+        } catch (UrlNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+}
