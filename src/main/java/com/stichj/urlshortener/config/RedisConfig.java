3e70bf27ac4faffcd169abef45e3338f9f035dc2
stichj <jonas.stich@tum.de>
2025-04-30 17:01:54 +0200

    Add Redis configuraiton to set correct host and port

---
+++ src/main/java/com/stichj/urlshortener/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.stichj.urlshortener.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(System.getenv("SPRING_REDIS_HOST"));
+        redisConfig.setPort(Integer.parseInt(System.getenv("SPRING_REDIS_PORT")));
+        return new LettuceConnectionFactory(redisConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+}
+
